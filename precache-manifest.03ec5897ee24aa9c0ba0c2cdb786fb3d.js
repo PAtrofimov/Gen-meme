e21c04a1017e6c06488f6dc573e91e459e003520
Peter Trofimov <ptrofimov7@gmail.com>
2020-12-20 13:43:40 +0200

    Updates

---
+++ precache-manifest.03ec5897ee24aa9c0ba0c2cdb786fb3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88bb7b793956475e3810056425f9bb69",
+    "url": "/Gen-meme/index.html"
+  },
+  {
+    "revision": "1fa77a5f597a24a13626",
+    "url": "/Gen-meme/static/css/main.6d0b9498.chunk.css"
+  },
+  {
+    "revision": "0354aa53667b0aafffba",
+    "url": "/Gen-meme/static/js/2.267ef1b6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Gen-meme/static/js/2.267ef1b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa77a5f597a24a13626",
+    "url": "/Gen-meme/static/js/main.df36d1fa.chunk.js"
+  },
+  {
+    "revision": "f7a838ff5465a1a6a155",
+    "url": "/Gen-meme/static/js/runtime-main.beccd56f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Gen-meme/precache-manifest.03ec5897ee24aa9c0ba0c2cdb786fb3d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Gen-meme/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
